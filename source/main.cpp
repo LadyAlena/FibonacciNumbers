784ecd10f5cc8815d3cbb47a156b5e1f8376d1ed
Елена Степанова <lady.alena-he2015@yandex.ru>
2022-10-09 18:56:00 +0300

    added a check for the correctness of the input

--- source/main.cpp
+++ source/main.cpp
@@ -7,6 +7,11 @@ int main(int argc, char** argv) {
 	std::cout << "Введите число: ";
 	std::cin >> n;
 
+	if (n <= 0) {
+		std::cout << "Количество чисел Фибоначчи не может быть меньше или равно 0" << std::endl;
+		return 1;
+	}
+
 	for (int i = 0; i < n; ++i) {
 		FibNumbers fibNums(i);
 		std::cout << fibNums.getFib(i) << (i < n - 1 ? " ": "\n");
